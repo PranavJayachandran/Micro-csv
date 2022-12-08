a4b1a6bd5ecd58b54ec9779ef689c2b483d99936
Pranav Jayachandran <pranavjay02@gmail.com>
2022-12-08 21:02:54 +0530

    adding

--- src/client/dialog-demo-tailwindcss/components/SheetEditor.jsx
+++ src/client/dialog-demo-tailwindcss/components/SheetEditor.jsx
@@ -7,6 +7,8 @@ import Papa from "papaparse";
 import { serverFunctions } from '../../utils/serverFunctions';
 
 const allowedExtension = ["csv"];
+
+
 const SheetEditor = () => {
   const [names, setNames] = useState([]);
 
@@ -22,8 +24,11 @@ const SheetEditor = () => {
   const setActiveSheet = (sheetName) => {
     serverFunctions.setActiveSheet(sheetName).then(setNames).catch(alert);
   };
+
+  const [datax, setDatax] = useState([]);
+  var data = [];
   const addData = async () => {
-    var data = [];
+    data = [];
     var temp = [];
     console.log(array, headers, map1);
     for (var i = 0; i < array.length; i++) {
@@ -34,14 +39,50 @@ const SheetEditor = () => {
           t += "\r"; temp.push(array[i][t]);
         }
         else {
-          console.log(activeheader[j], map1.get(activeheader[j]), array[i][map1.get(activeheader[j]).toString()]); temp.push(array[i][map1.get(activeheader[j])]);
+          temp.push(array[i][map1.get(activeheader[j])]);
         }
       }
+
       data.push(temp);
     }
-    console.log(data);
-    serverFunctions.setData(data);
+    setDatax(data);
+    setK(1);
+  }
+  const pushData = () => {
+    serverFunctions.setData(datax);
+  }
+  const showPreview = () => {
+    addData();
+    setShowFinalData(1);
+    setShowImport(0);
+  }
 
+  const [k, setK] = useState(0);
+  const Preview = () => {
+    useEffect(() => {
+
+    })
+    const handle1 = () => {
+      console.log("Submit");
+      pushData();
+    }
+    return (
+      <div>
+        <div className='flex flex-col mt-10 items-center'>
+          {datax.map((item) => (
+            <div className='flex gap-10'>
+              {item.map((o) => (
+                <div>{o}</div>
+              ))}
+            </div>
+          )
+          )}
+        </div>
+        <div onClick={handle1} className="mt-5">
+          Sumbit
+        </div>
+      </div>
+    )
   }
 
   // You can also use async/await notation for server calls with our server wrapper.
@@ -61,6 +102,8 @@ const SheetEditor = () => {
   const [headers, setHeader] = useState([]);
   const [activeheader, setActiveHeader] = useState([]);
   const [dataloaded, setDataLoaded] = useState(0);
+  const [showfinaldata, setShowFinalData] = useState(0);
+  const [showimport, setShowImport] = useState(1);
 
   const fileReader = new FileReader();
 
@@ -104,17 +147,30 @@ const SheetEditor = () => {
   };
   const map1 = new Map();
   const Mapping = (props) => {
-    map1.set(activeheader[props.index], headers[0],);
+    map1.set(activeheader[props.index], " ");
+    for (let i = 0; i < headers.length; i++) {
+      if (activeheader[props.index].toLowerCase() == headers[i].toLowerCase()) {
+        console.log(activeheader[props.index].toLowerCase(), headers[i].toLowerCase());
+        map1.set(activeheader[props.index], headers[i],);
+      }
+    }
+
+    console.log(map1);
     const handle = (e) => {
       map1.set(activeheader[props.index], e.target.value,);
     }
+
     return (
       <div>
         <label for="cars">Choose maping for {activeheader[props.index]}</label>
         <select onChange={handle}>
+          <option>{map1.get(activeheader[props.index])}</option>
           {headers.map((item, index) =>
           (
-            <option>{item}</option>
+            item != map1.get(activeheader[props.index]) ?
+              <option>{item}</option>
+              : <>
+              </>
           )
           )
           }
@@ -128,54 +184,63 @@ const SheetEditor = () => {
   const headerKeys = Object.keys(Object.assign({}, ...array));
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <form>
-        <input
-          type={"file"}
-          id={"csvFileInput"}
-          accept={".csv"}
-          onChange={handleOnChange}
-        />
+    <div>
+      <div style={{ textAlign: "center" }}>
+        {showimport ? <div><form>
+          <input
+            type={"file"}
+            id={"csvFileInput"}
+            accept={".csv"}
+            onChange={handleOnChange}
+          />
 
-        <button
-          onClick={(e) => {
-            handleOnSubmit(e);
-          }}
-        >
-          IMPORT CSV
-        </button>
-      </form>
+          <button
+            onClick={(e) => {
+              handleOnSubmit(e);
+            }}
+          >
+            IMPORT CSV
+          </button>
+        </form>
 
-      <br />
+          <br />
 
-      <table>
-        <thead>
-          <tr key={"header"}>
-            {headerKeys.map((key) => (
-              <th>{key}</th>
-            ))}
-          </tr>
-        </thead>
+          <table>
+            <thead>
+              <tr key={"header"}>
+                {headerKeys.map((key) => (
+                  <th>{key}</th>
+                ))}
+              </tr>
+            </thead>
 
-        <tbody>
-          {array.map((item) => (
-            <tr key={item.id}>
-              {Object.values(item).map((val) => (
-                <td>{val}</td>
+            <tbody>
+              {array.map((item) => (
+                <tr key={item.id}>
+                  {Object.values(item).map((val) => (
+                    <td>{val}</td>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button onClick={addData}>
-        Add
-      </button>
-      <div className='mt-10'>
-        {activeheader.map((item, index) => (
-          <div>
-            <Mapping index={index} />
+            </tbody>
+          </table>
+          <button onClick={showPreview}>
+            Add
+          </button>
+
+
+          <div className='mt-10'>
+            {activeheader.map((item, index) => (
+              <div>
+                <Mapping index={index} />
+              </div>
+            ))}
           </div>
-        ))}
+        </div> :
+          <></>
+        }
+
+        {showfinaldata ? <Preview /> : <></>}
       </div>
     </div>
   );
